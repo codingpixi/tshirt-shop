63cbcff7f1854b07b0ebc28157a23349dde70546
ddsheard <ddsheard@gmail.com>
2017-05-18 12:58:54 -0400

    2_1 complete

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ function logEvent() {
   console.log('event'); //event was in parenthasis without signle quotes
 }
 
-let $allColors = $(".selectColor");
+let $allColors = $("div.colors div");
 $allColors.on("click", changeColors);
 function logEvent() {
   console.log('event'); //event was in parenthasis without signle quotes
@@ -27,8 +27,8 @@ function addToCart() {
   $shirtPrices.text(totalPrice);
 };
 
-each shirt var set = 0
-++ when = 7 disable
+// each shirt var set = 0
+// ++ when = 7 disable
 //tshirt size changes in box
 // $('.submenuSize').on('click', changeSize);
 // function changeSize() {
@@ -43,9 +43,16 @@ function changeSizes(event) {
   sizeBox.textContent = size;
 }
 
+//changeColorForShirtAndBox
 function changeColors() {
+
+    ($(this).parent().parent().parent().prev().find('.frontShirt').children().attr('src', `assets/${$(this).attr('class')}-front.png`))
+
     if ($(this).css('background-color') == 'rgb(129, 188, 230)') {
       $(this).closest('.colors').siblings().attr('class', 'blue');
+      // CHANGE SHIRT SOURCE FILE
+      //  $('.changeSrc').attr('src','assets/blue-front.png');
+        // $(this)
     } else if ($(this).css('background-color') == 'rgb(226, 117, 167)') {
       $(this).closest('.colors').siblings().attr('class', 'pink');
     } else if ($(this).css('background-color') == 'rgb(229, 177, 120)') {
