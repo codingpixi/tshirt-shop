a74e22b069e96502d74166d4c8f8e14177552d85
ddsheard <ddsheard@gmail.com>
2017-05-18 21:22:08 -0400

    almost done

--- javascript.js
+++ javascript.js
@@ -11,30 +11,86 @@ function logEvent() {
   console.log('event'); //event was in parenthasis without signle quotes
 }
 
+let shirtArr = []
+
 let totalPrice = 0;
+
+let totalPrices =[]
 
 //Adding tshirts and price to shopping cart
 $('.shopCart').on('click', addToCart);
 
+//(this) means the the thing that was clicked - which is the shopping cart
+
 function addToCart() {
-  console.log($(this).parent().next().text());
-  let $ul = $('ul');
-  let $li = $(`<li>${$(this).parent().next().text()}</li>`);
-  $ul.append($li);
-  let $shirtPrices = $('#totalPrice');
-  let $pickles = (`${$(this).parent().next().find('.price').text()}`);
-  totalPrice = totalPrice + parseInt($pickles);
-  $shirtPrices.text(totalPrice);
+  let item = {
+    name: $(this).parent().next().find('p').text(),
+    price: $(this).parent().next().find('span').text()
+  }
+  let maxShirt = shirtArr.filter(obj => obj === item.name)
+  console.log(maxShirt.length);
+  console.log(maxShirt);
+  console.log(shirtArr);
+  if (maxShirt.length <=7) {
+    console.log($(this).parent().next().text());
+    console.log(this);
+    let $ul = $('ul');
+    let $li = $(`<li>${$(this).parent().next().text()}</li>`);
+    $ul.append($li);
+
+    shirtArr.push(item.name)
+    console.log(item.name);
+    console.log(shirtArr);
+
+    totalPrices.push(item.price)
+    console.log(totalPrices);
+    console.log(item.price);
+
+    let $shirtPrices = $('#totalPrice');
+    let $priceIsPickles = (`${$(this).parent().next().find('.price').text()}`);
+    console.log($shirtPrices);
+    console.log($priceIsPickles);
+    totalPrice = totalPrice + parseInt($priceIsPickles);
+    $shirtPrices.text(totalPrice);
+
+
+    if (totalPrices.length >= 5) {
+      let totalPrice = $('#totalPrice').text()
+      let discount = totalPrice * .05
+      $('#discount').text('Discount ' + discount)
+      console.log(totalPrices);
+      let newPrice = totalPrices - discount
+      $('#newPrice').text('NewPrice ' + newPrice)
+    }
+
+
+
+
+
+  }
+
 };
+
+
+
+// SHIRTS 5% DISCOUNT
+// function discount() {
+
+//   let $disc = totalPrice - $discAmount;
+//   if (shirtArr.length >= 5) {
+//     (.'price') * .05 - totalPrice
+//   } else () {
+//
+//   }
+// }
+
+
+
+
+// MAX PURCHASE 8 PER SHIRT
 
 // each shirt var set = 0
 // ++ when = 7 disable
-//tshirt size changes in box
-// $('.submenuSize').on('click', changeSize);
-// function changeSize() {
-//   let $div = $('div');
-//   console.log($(this).text());
-// }
 
 //Change Size
 function changeSizes(event) {
